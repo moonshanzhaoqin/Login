5a2fa350cc187db91c566e04dfd7723293dcc808
suzan.wu <suzan.wu@yuyutechnology.com>
2016-12-09 15:46:29 +0800

    retcode  去掉下划线

--- common-dal/src/main/java/com/yuyutechnology/exchange/ServerConsts.java
+++ common-dal/src/main/java/com/yuyutechnology/exchange/ServerConsts.java
@@ -7,80 +7,80 @@ public class ServerConsts {
 	/**
 	 * 成功
 	 */
-	public static final String RET_CODE_SUCCESS = "00_000";
+	public static final String RET_CODE_SUCCESS = "00000";
 	/**
 	 * 失败
 	 */
-	public static final String RET_CODE_FAILUE = "00_001";
+	public static final String RET_CODE_FAILUE = "00001";
 
 	/**
 	 * Session过期
 	 */
-	public static final String SESSION_TIMEOUT = "00_002";
+	public static final String SESSION_TIMEOUT = "00002";
 	/**
 	 * 参数为空
 	 */
-	public static final String PARAMETER_IS_EMPTY = "00_003";
+	public static final String PARAMETER_IS_EMPTY = "00003";
 
 	// 用户 01
 	/**
 	 * 手机号已注册
 	 */
-	public static final String PHONE_IS_REGISTERED = "01_001";
+	public static final String PHONE_IS_REGISTERED = "01001";
 	/**
 	 * 手机号不存在
 	 */
-	public static final String PHONE_NOT_EXIST = "01_002";
+	public static final String PHONE_NOT_EXIST = "01002";
 	/**
 	 * 手机号与验证码不匹配
 	 */
-	public static final String PHONE_AND_CODE_NOT_MATCH = "01_003";
+	public static final String PHONE_AND_CODE_NOT_MATCH = "01003";
 	/**
 	 * goldpay没有绑定手机
 	 */
-	public static final String GOLDPAY_PHONE_IS_NOT_EXIST = "01_004";
+	public static final String GOLDPAY_PHONE_IS_NOT_EXIST = "01004";
 	/**
 	 * goldpay手机号与注册手机号不匹配
 	 */
-	public static final String GOLDPAY_PHONE_NOT_MATCH = "01_005";
+	public static final String GOLDPAY_PHONE_NOT_MATCH = "01005";
 	/**
 	 * 支付密码不是6位数字
 	 */
-	public static final String PAY_PASSWORD_IS_ILLEGAL = "01_006";
+	public static final String PAY_PASSWORD_IS_ILLEGAL = "01006";
 	/**
 	 * 手机号与密码不匹配
 	 */
-	public static final String PHONE_AND_PASSWORD_NOT_MATCH = "01_006";
+	public static final String PHONE_AND_PASSWORD_NOT_MATCH = "01006";
 
 	// 兑换 02
 	/**
 	 * 查询不到该用户的wallet信息
 	 */
-	public static final String EXCHANGE_WALLET_CAN_NOT_BE_QUERIED = "02_001";
+	public static final String EXCHANGE_WALLET_CAN_NOT_BE_QUERIED = "02001";
 	/**
 	 * 兑换金额大于余额
 	 */
-	public static final String EXCHANGE_OUTPUTAMOUNT_BIGGER_THAN_BALANCE = "02_002";
+	public static final String EXCHANGE_OUTPUTAMOUNT_BIGGER_THAN_BALANCE = "02002";
 	/**
 	 * 换算后金额低于最小限额
 	 */
-	public static final String EXCHANGE_AMOUNT_LESS_THAN_MINIMUM_TRANSACTION_AMOUNT = "02_003";
+	public static final String EXCHANGE_AMOUNT_LESS_THAN_MINIMUM_TRANSACTION_AMOUNT = "02003";
 
 	// 交易03
 	/**
 	 * 余额不足
 	 */
-	public static final String TRANSFER_CURRENT_BALANCE_INSUFFICIENT = "03_001";
+	public static final String TRANSFER_CURRENT_BALANCE_INSUFFICIENT = "03001";
 	/**
 	 * 超过当日最大限额
 	 */
-	public static final String TRANSFER_EXCEEDED_TRANSACTION_LIMIT = "03_002";
+	public static final String TRANSFER_EXCEEDED_TRANSACTION_LIMIT = "03002";
 	/**
 	 * 支付密码不正确
 	 */
-	public static final String TRANSFER_PAYMENTPWD_INCORRECT = "03_003";
+	public static final String TRANSFER_PAYMENTPWD_INCORRECT = "03003";
 
-	public static final String TRANSFER_REQUIRES_PHONE_VERIFICATION = "03_004";
+	public static final String TRANSFER_REQUIRES_PHONE_VERIFICATION = "03004";
 
 	// 用户类型
 	/**
