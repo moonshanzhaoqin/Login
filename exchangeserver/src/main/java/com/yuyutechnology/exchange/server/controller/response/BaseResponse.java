2c4da2b68d6e64dd208ffc9dea8b3e89eb002cea
silent.sun <silent.sun@aspectgaming.com>
2016-12-14 18:22:20 +0800

    Signed-off-by: silent.sun <silent.sun@aspectgaming.com>

--- exchangeserver/src/main/java/com/yuyutechnology/exchange/server/controller/response/BaseResponse.java
+++ exchangeserver/src/main/java/com/yuyutechnology/exchange/server/controller/response/BaseResponse.java
@@ -3,6 +3,15 @@ package com.yuyutechnology.exchange.server.controller.response;
 public class BaseResponse {
 	private String retCode;
 	private String message;
+	private String apiName;
+
+	public BaseResponse() {
+		super();
+		String name = this.getClass().getSimpleName();
+		name = name.substring(0, name.indexOf("Response"));
+		name = name.substring(0,1).toLowerCase() + name.substring(1);
+		apiName = name;
+	}
 
 	public String getRetCode() {
 		return retCode;
@@ -19,4 +28,9 @@ public class BaseResponse {
 	public void setMessage(String message) {
 		this.message = message;
 	}
+
+	public String getApiName() {
+		return apiName;
+	}
 }
+
