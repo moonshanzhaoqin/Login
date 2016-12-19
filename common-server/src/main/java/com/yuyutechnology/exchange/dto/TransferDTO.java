4547dede1442e3bf87f1f7a7f1316a09f1efd273
nicholas.chi <nicholas.chi@aspectgaming.com>
2016-12-19 09:59:16 +0800

    Signed-off-by: nicholas.chi <nicholas.chi@aspectgaming.com>

---
+++ common-server/src/main/java/com/yuyutechnology/exchange/dto/TransferDTO.java
@@ -0,0 +1,59 @@
+package com.yuyutechnology.exchange.dto;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class TransferDTO {
+	
+//	private int userId;
+	private String currency;
+	private BigDecimal amount;
+	private String phoneNum;
+	private String comments;
+	private Date finishAt;
+	private int transferType;
+	
+//	public int getUserId() {
+//		return userId;
+//	}
+//	public void setUserId(int userId) {
+//		this.userId = userId;
+//	}
+	public String getCurrency() {
+		return currency;
+	}
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+	public BigDecimal getAmount() {
+		return amount;
+	}
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+	public String getComments() {
+		return comments;
+	}
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+	public Date getFinishAt() {
+		return finishAt;
+	}
+	public void setFinishAt(Date finishAt) {
+		this.finishAt = finishAt;
+	}
+	public int getTransferType() {
+		return transferType;
+	}
+	public void setTransferType(int transferType) {
+		this.transferType = transferType;
+	}
+
+}
