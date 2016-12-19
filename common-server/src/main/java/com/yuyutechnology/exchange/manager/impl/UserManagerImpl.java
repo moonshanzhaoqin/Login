875e83dd17bfeb9f03c86348c22acf44848d74d8
suzan.wu <suzan.wu@yuyutechnology.com>
2016-12-19 17:36:14 +0800

    Signed-off-by: suzan.wu <suzan.wu@yuyutechnology.com>

--- common-server/src/main/java/com/yuyutechnology/exchange/manager/impl/UserManagerImpl.java
+++ common-server/src/main/java/com/yuyutechnology/exchange/manager/impl/UserManagerImpl.java
@@ -1,20 +1,16 @@
 package com.yuyutechnology.exchange.manager.impl;
 
-import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
-
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.yuyutechnology.exchange.ServerConsts;
