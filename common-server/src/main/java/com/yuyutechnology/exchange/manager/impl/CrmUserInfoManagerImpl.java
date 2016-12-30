0b9c6d93ed17b1e60cf2be548c82855ec1168df7
nicholas.chi <nicholas.chi@aspectgaming.com>
2016-12-30 10:24:46 +0800

    系统和用户总账户信息概览
    Signed-off-by: nicholas.chi <nicholas.chi@aspectgaming.com>

--- common-server/src/main/java/com/yuyutechnology/exchange/manager/impl/CrmUserInfoManagerImpl.java
+++ common-server/src/main/java/com/yuyutechnology/exchange/manager/impl/CrmUserInfoManagerImpl.java
@@ -1,6 +1,7 @@
 package com.yuyutechnology.exchange.manager.impl;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -87,7 +88,7 @@ public class CrmUserInfoManagerImpl implements CrmUserInfoManager {
 			}
 		}
 		
-		map.put("totalAssets", totalAssets);
+		map.put("totalAssets", totalAssets.setScale(2, RoundingMode.CEILING));
 		
 		return map;
 		
