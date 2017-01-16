5c8f06de18d94ad05c7fbe7f9417d83332b97a1a
silent.sun <silent.sun@aspectgaming.com>
2017-01-16 11:01:45 +0800

    汇率更新时间
    Signed-off-by: silent.sun <silent.sun@aspectgaming.com>

--- exchangeserver/src/main/java/com/yuyutechnology/exchange/server/controller/response/GetExchangeRateResponse.java
+++ exchangeserver/src/main/java/com/yuyutechnology/exchange/server/controller/response/GetExchangeRateResponse.java
@@ -1,10 +1,12 @@
 package com.yuyutechnology.exchange.server.controller.response;
 
+import java.util.Date;
 import java.util.HashMap;
 
 public class GetExchangeRateResponse extends BaseResponse {
 	
 	private String base;
+	private Date rateUpdateTime;
 	private HashMap<String, Double> exchangeRates;
 	
 	public String getBase() {
@@ -19,5 +21,10 @@ public class GetExchangeRateResponse extends BaseResponse {
 	public void setExchangeRates(HashMap<String, Double> exchangeRates) {
 		this.exchangeRates = exchangeRates;
 	}
-
+	public Date getRateUpdateTime() {
+		return rateUpdateTime;
+	}
+	public void setRateUpdateTime(Date rateUpdateTime) {
+		this.rateUpdateTime = rateUpdateTime;
+	}
 }
