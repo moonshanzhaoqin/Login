b8508eb7a7469042bb0b4b4a47dc88fd2aadcc99
silent.sun <silent.sun@aspectgaming.com>
2017-03-15 17:35:06 +0800

    优化httpclient
    Signed-off-by: silent.sun <silent.sun@aspectgaming.com>

--- common-dal/src/main/java/com/yuyutechnology/exchange/utils/HttpClientUtils.java
+++ common-dal/src/main/java/com/yuyutechnology/exchange/utils/HttpClientUtils.java
@@ -1,18 +1,15 @@
 package com.yuyutechnology.exchange.utils;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
@@ -22,90 +19,59 @@ public class HttpClientUtils {
 	
 	public static Logger logger = LoggerFactory.getLogger(HttpClientUtils.class);
 	
-	@SuppressWarnings("deprecation")
-	public static String sendGet(String domain,String params){
-		
-		String result = "";
-		
-		HttpClient httpClient = HttpClientBuilder.create().build();
-        try {
-        	String urlName = domain;
-        	if(StringUtils.isNotBlank(params)){
-        		urlName = domain + "?" + params;
-        	}
-            HttpUriRequest httpGet = new HttpGet(urlName);
-//            httpGet.setHeader(new BasicHeader("Authorization", "Bearer " + access_token));
-            logger.info("Executing request: {}",httpGet.getRequestLine());
-            HttpResponse resp = httpClient.execute(httpGet);
-            HttpEntity entity = resp.getEntity();
-            if (resp.getStatusLine().getStatusCode() == 200 && entity != null) {
-                InputStream stream = entity.getContent();
-                String line;
-                BufferedReader br = new BufferedReader(new InputStreamReader(stream));
-                while ((line = br.readLine()) != null) {
-                	result += "" + line;
-                }
-            } else {
-                // print error message
-                String responseString = EntityUtils.toString(entity, "UTF-8");
-                logger.info("responseString : {}",responseString);
-            }
-
-        }catch(ClientProtocolException e1){
-        	result = e1.getMessage();
-        	e1.printStackTrace();
-        }catch(IOException e2){
-        	result = e2.getMessage();
-        	e2.printStackTrace();
-        }finally {
-            httpClient.getConnectionManager().shutdown();
-        }
-        
-        logger.info("HttpClientUtils result : {}",result);
-        
-		return result;
+	private static Object lock = new Object();
+	private static CloseableHttpClient client;
+	
+	private static CloseableHttpClient getClient() {
+		if (client == null) {
+			synchronized (lock)
+			{
+				if (client == null)
+				{
+					PoolingHttpClientConnectionManager cm =new PoolingHttpClientConnectionManager();
+			        cm.setMaxTotal(50);
+			        cm.setDefaultMaxPerRoute(20);
+					RequestConfig config = RequestConfig.custom()
+			                .setConnectTimeout(3000) 
+			                .setSocketTimeout(3000).build();
+					client = HttpClients.custom().setConnectionManager(cm).setDefaultRequestConfig(config).disableAutomaticRetries().build();
+				}
+			}
+		}
+		return client;
 	}
 	
-	@SuppressWarnings("deprecation")
 	public static String sendGet(String domain,String params,BasicHeader basicHeader){
+		String urlName = domain;
+		if(StringUtils.isNotBlank(params)){
+			urlName = domain + "?" + params;
+		}
 		String result = "";
-		HttpClient httpClient = HttpClientBuilder.create().build();
+		CloseableHttpResponse resp=null;
         try {
-        	String urlName = domain;
-        	if(StringUtils.isNotBlank(params)){
-        		urlName = domain + "?" + params;
-        	}
-            HttpUriRequest httpGet = new HttpGet(urlName);
+        	HttpGet httpGet = new HttpGet(urlName);
             httpGet.setHeader(basicHeader);
-//            logger.info("Executing request: {}",httpGet.getRequestLine());
-            HttpResponse resp = httpClient.execute(httpGet);
+            logger.warn("sendGet url : {}, execute",urlName);
+            resp = getClient().execute(httpGet);
+            logger.warn("sendGet url : {},  resp",urlName);
             HttpEntity entity = resp.getEntity();
             if (resp.getStatusLine().getStatusCode() == 200 && entity != null) {
-                InputStream stream = entity.getContent();
-                String line;
-                BufferedReader br = new BufferedReader(new InputStreamReader(stream));
-                while ((line = br.readLine()) != null) {
-                	result += "" + line;
-                }
+                result = EntityUtils.toString(entity, "UTF-8");  
+                logger.warn("sendGet url : {},  result : {}",urlName, result);
             } else {
-                // print error message
                 String responseString = EntityUtils.toString(entity, "UTF-8");
-                result = responseString;
-//                logger.info("responseString : {}",responseString);
+                logger.warn("sendGet url : {},  result : {}",urlName, responseString);
             }
-
-        }catch(ClientProtocolException e1){
-        	result = e1.getMessage();
-        	e1.printStackTrace();
-        }catch(IOException e2){
-        	result = e2.getMessage();
-        	e2.printStackTrace();
+        }catch(Exception e){
+        	logger.warn("sendGet url : {},  result : {}",urlName, e.getMessage());
         }finally {
-            httpClient.getConnectionManager().shutdown();
+        	 if(resp!=null){
+        		 try {
+        			 resp.close();
+        		 } catch (IOException e) {
+        		 }
+        	 }
         }
-        
-//        logger.info("HttpClientUtils result : {}",result);
-        
 		return result;
 	}
 	
