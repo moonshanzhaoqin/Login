23928120db92989960bcc211ff2a89a373694f49
suzan.wu <suzan.wu@aspectgaming.com>
2017-03-20 11:25:03 +0800

    db Signed-off-by: suzan.wu <suzan.wu@aspectgaming.com>

--- db/db_change_1.2.2.sql
+++ db/db_change_1.2.2.sql
@@ -1,5 +1,5 @@
 ﻿# Host: 10.1.1.66  (Version 5.6.19-0ubuntu0.14.04.1)
-# Date: 2017-03-08 10:01:37
+# Date: 2017-03-20 11:23:12
 # Generator: MySQL-Front 6.0  (Build 1.11)
 
 /*!40101 SET NAMES utf8 */;
@@ -12,8 +12,10 @@ DROP TABLE IF EXISTS `e_withdraw`;
 CREATE TABLE `e_withdraw` (
   `withdraw_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL DEFAULT '0' COMMENT '用户ID',
+  `area_code` varchar(5) NOT NULL DEFAULT '' COMMENT '国家码',
+  `user_phone` varchar(255) NOT NULL DEFAULT '' COMMENT '用户手机号',
   `transfer_id` varchar(255) NOT NULL DEFAULT '0' COMMENT '转账ID',
-  `review_status` int(1) NOT NULL DEFAULT '0' COMMENT '审核状态 未审核/通过/未通过',
-  `goldpay_remit` int(1) NOT NULL DEFAULT '0' COMMENT 'Goldpay划账结果 未执行/成功/失败',
+  `review_status` int(1) NOT NULL DEFAULT '0' COMMENT '审核状态 未审核/未通过/通过',
+  `goldpay_remit` int(1) NOT NULL DEFAULT '0' COMMENT 'Goldpay划账结果 未执行/失败/成功',
   PRIMARY KEY (`withdraw_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='提现审核';
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8 COMMENT='提现审核';
