6b7f5cf0b17e4980525c7ea1dbf80d3d6af3d9cd
silent.sun <silent.sun@aspectgaming.com>
2017-03-22 17:28:56 +0800

    Signed-off-by: silent.sun <silent.sun@aspectgaming.com>

--- db/db_change_1.2.3.sql
+++ db/db_change_1.2.3.sql
@@ -2,4 +2,30 @@ alter table `e_wallet` add column `update_seq_id` int (11) DEFAULT '0' NOT NULL 
 
 update e_wallet w left join (select MAX(seq_id) as seq_id,user_id,currency from e_wallet_seq group by user_id, currency) s on w.user_id = s.user_id and w.currency = s.currency set w.update_seq_id = s.seq_id;
 
+alter table `e_wallet` add index `index_update_time` (`update_time`);
 
+CREATE TABLE `e_wallet_history` (                                      
+    `user_id` int(11) NOT NULL DEFAULT '0' COMMENT '用户ID',           
+    `currency` char(3) NOT NULL DEFAULT '' COMMENT '币种',             
+    `balance` decimal(20,4) NOT NULL DEFAULT '0.0000' COMMENT '余额',  
+    `update_time` datetime DEFAULT NULL COMMENT '最新变动时间',    
+    `update_seq_id` int(11) NOT NULL DEFAULT '0',                        
+    PRIMARY KEY (`user_id`,`currency`)                                   
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='钱包' 
+                  
+CREATE TABLE `e_wallet_now` (                                      
+    `user_id` int(11) NOT NULL DEFAULT '0' COMMENT '用户ID',           
+    `currency` char(3) NOT NULL DEFAULT '' COMMENT '币种',             
+    `balance` decimal(20,4) NOT NULL DEFAULT '0.0000' COMMENT '余额',  
+    `update_time` datetime DEFAULT NULL COMMENT '最新变动时间',    
+    `update_seq_id` int(11) NOT NULL DEFAULT '0',                        
+    PRIMARY KEY (`user_id`,`currency`)                                   
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='钱包' 
+
+alter table `e_wallet_history` add index `index_update_time` (`update_time`);
+
+alter table `e_wallet_history` add index `index_update_seq_id` (`update_seq_id`);
+
+alter table `e_wallet_now` add index `index_update_time` (`update_time`);
+
+alter table `e_wallet_now` add index `index_update_seq_id` (`update_seq_id`);
