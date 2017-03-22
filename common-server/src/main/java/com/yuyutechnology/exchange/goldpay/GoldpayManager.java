7469f8c889110d14679e5f5302f4984412661de4
silent.sun <silent.sun@aspectgaming.com>
2017-03-22 17:48:00 +0800

    Signed-off-by: silent.sun <silent.sun@aspectgaming.com>

--- common-server/src/main/java/com/yuyutechnology/exchange/goldpay/GoldpayManager.java
+++ common-server/src/main/java/com/yuyutechnology/exchange/goldpay/GoldpayManager.java
@@ -1,5 +1,13 @@
 package com.yuyutechnology.exchange.goldpay;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -36,7 +44,7 @@ public class GoldpayManager {
 		param.append("grant_type=").append("password").append("&");
 		param.append("username=").append(userName).append("&");
 		param.append("password=").append(password);
-		String result = HttpTookit.sendPost4Form(ResourceUtils.getBundleValue4String("goldpayUas.url") + "oauth/token", param.toString());
+		String result = sendPost(ResourceUtils.getBundleValue4String("goldpayUas.url") + "oauth/token", param.toString());
 		logger.info("result==={}", result);
 		if (StringUtils.isNotEmpty(result)) {
 			Map<String, String> resultMap = JsonBinder.getInstance().fromJson(result, HashMap.class);
@@ -45,5 +53,49 @@ public class GoldpayManager {
 			}
 		}
 		return null;
+	}
+	
+	private String sendPost(String link, String param){
+		BufferedWriter out = null;
+		BufferedReader in = null;
+		String body = "";
+		try {
+			URL url = new URL(link);// 创建连接
+			HttpURLConnection connection = (HttpURLConnection) url
+					.openConnection();
+			connection.setDoOutput(true);
+			connection.setDoInput(true);
+			connection.setUseCaches(false);
+			connection.setInstanceFollowRedirects(true);
+			connection.setRequestMethod("POST"); // 设置请求方式
+			connection.setRequestProperty("Accept", "application/json"); // 设置接收数据的格式
+			connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded"); // 设置发送数据的格式
+			connection.connect();
+			out = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream(), "UTF-8"));
+			out.write(param);
+			out.flush();
+			// 读取响应
+			InputStream jnn = connection.getInputStream();
+			in = new BufferedReader(new InputStreamReader(jnn, "UTF-8"));
+			String line = "";
+			while ((line = in.readLine()) != null) {
+				body += "" + line;
+			}
+			if (connection.getResponseCode() == 200) {
+				return body;
+			}
+		} catch (IOException e) {
+			logger.warn("send to uas check failed {}",e.getMessage());
+		} finally { // 关闭流
+			try {
+				if (out != null) {
+					out.close();
+				}
+			} catch (IOException ex) {
+				logger.error("close in or out io failed");
+				ex.printStackTrace();
+			}
+		}
+		return body;
 	}
 }
