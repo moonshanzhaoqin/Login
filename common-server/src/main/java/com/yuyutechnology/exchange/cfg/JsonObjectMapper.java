3efa4dd64a09e6b2b3010153725a4c0ec1fb69a3
silent.sun <silent.sun@aspectgaming.com>
2017-05-18 11:56:11 +0800

    字段为null时修复Json转换错误
    Signed-off-by: silent.sun <silent.sun@aspectgaming.com>

--- common-server/src/main/java/com/yuyutechnology/exchange/cfg/JsonObjectMapper.java
+++ common-server/src/main/java/com/yuyutechnology/exchange/cfg/JsonObjectMapper.java
@@ -3,13 +3,8 @@
  */
 package com.yuyutechnology.exchange.cfg;
 
-import java.io.IOException;
-
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializerProvider;
 
 /**
  * @author silent.sun
@@ -24,14 +19,15 @@ public class JsonObjectMapper extends ObjectMapper {
 
     public JsonObjectMapper(){
         super();
-        // 空值处理为空串  
-        this.getSerializerProvider().setNullValueSerializer(new JsonSerializer<Object>() {  
-            @Override  
-            public void serialize(Object value, JsonGenerator jg, SerializerProvider sp) throws IOException, JsonProcessingException {  
-            	if (value instanceof String) {
-            		jg.writeString("");  
-            	}
-            }  
-        });
+        this.setSerializationInclusion(Include.NON_EMPTY);
+//        // 空值处理为空串  
+//        this.getSerializerProvider().setNullValueSerializer(new JsonSerializer<Object>() {  
+//            @Override  
+//            public void serialize(Object value, JsonGenerator jg, SerializerProvider sp) throws IOException, JsonProcessingException {  
+//            	if (value instanceof String) {
+//            		jg.writeString("");  
+//            	}
+//            }  
+//        });
     } 
 }
