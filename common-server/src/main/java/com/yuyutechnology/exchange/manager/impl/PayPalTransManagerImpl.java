70f70e90e36a4b2333a7f5f87d59e8ac62bc7e5d
silent.sun <silent.sun@aspectgaming.com>
2017-06-13 16:39:33 +0800

    Signed-off-by: silent.sun <silent.sun@aspectgaming.com>

--- common-server/src/main/java/com/yuyutechnology/exchange/manager/impl/PayPalTransManagerImpl.java
+++ common-server/src/main/java/com/yuyutechnology/exchange/manager/impl/PayPalTransManagerImpl.java
@@ -148,7 +148,7 @@ public class PayPalTransManagerImpl implements PayPalTransManager {
 			request.merchantAccountId(transfer.getPaypalCurrency());
 			request.paymentMethodNonce(nonce);
 			request.orderId(transfer.getTransferId());
-//			request.options().submitForSettlement(true);
+			request.options().submitForSettlement(true);
 //			request.options().paypal().customField("PayPal custom field")
 //					.description("Description for PayPal email receipt").done();
 			request.options().storeInVaultOnSuccess(true).done();
