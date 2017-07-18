8eefcced40bc7f44c84f3d43c418a4035c9d9316
Niklaus Chi <nicholas.chi@aspectgaming.com>
2017-07-18 17:41:20 +0800

    debug 注册，并且接收转账邀请
    Signed-off-by: Niklaus Chi <nicholas.chi@aspectgaming.com>

--- exchangeserver/src/test/java/com/yuyutechnology/exchange/server/test/TransDetailsTest.java
+++ exchangeserver/src/test/java/com/yuyutechnology/exchange/server/test/TransDetailsTest.java
@@ -1,19 +1,13 @@
 package com.yuyutechnology.exchange.server.test;
 
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.yuyutechnology.exchange.ServerConsts;
 import com.yuyutechnology.exchange.dao.TransferDAO;
 import com.yuyutechnology.exchange.manager.CommonManager;
 import com.yuyutechnology.exchange.manager.OandaRatesManager;
 import com.yuyutechnology.exchange.manager.TransDetailsManager;
 import com.yuyutechnology.exchange.manager.TransferManager;
-import com.yuyutechnology.exchange.pojo.Currency;
 
 public class TransDetailsTest extends BaseSpringJunit4 {
 
@@ -35,18 +29,20 @@ public class TransDetailsTest extends BaseSpringJunit4 {
 //		TransDetailsDTO dto = transferManager.getTransDetails("2017042503120T000064",8);
 //		System.out.println(dto.getTraderName());
 		
-		List<Currency> list = commonManager.getCurrentCurrencies();
+//		List<Currency> list = commonManager.getCurrentCurrencies();
+//		
+//		for (Currency currency : list) {
+//			System.out.println(currency.getCurrency());
+//		}
+//		
+//		
+//		LinkedHashMap<String, Double> map = oandaRatesManager.getExchangeRate(ServerConsts.CURRENCY_OF_GOLDPAY);
+//		
+//		for (Map.Entry<String, Double> entry : map.entrySet()) {
+//			System.out.println(entry.getKey());
+//		}
 		
-		for (Currency currency : list) {
-			System.out.println(currency.getCurrency());
-		}
-		
-		
-		LinkedHashMap<String, Double> map = oandaRatesManager.getExchangeRate(ServerConsts.CURRENCY_OF_GOLDPAY);
-		
-		for (Map.Entry<String, Double> entry : map.entrySet()) {
-			System.out.println(entry.getKey());
-		}
+		transDetailsManager.updateTransDetailsWhenOtherOneRegist("2017042402440T000013", 2, "TEST");
 		
 	}
 	
