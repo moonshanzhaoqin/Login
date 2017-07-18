2521f541c31ca1dc06645a518ff6686da1fb5ef7
Niklaus Chi <nicholas.chi@aspectgaming.com>
2017-07-18 10:13:09 +0800

    type = 3 traderName = null
    Signed-off-by: Niklaus Chi <nicholas.chi@aspectgaming.com>

--- db/db_change_1.5.0.sql
+++ db/db_change_1.5.0.sql
@@ -82,7 +82,7 @@ t2.user_to = t3.user_id and t2.transfer_type = 2;
 INSERT INTO 
 e_trans_details(transfer_id,user_id,trader_name,trader_area_code,trader_phone,trans_currency,trans_amount,trans_remarks)
 SELECT 
-t2.transfer_id,t2.user_to,t3.user_name,
+t2.transfer_id,t2.user_to,NULL,
 t2.area_code,t2.phone,
 t2.currency,t2.transfer_amount,
 t2.transfer_comment 
