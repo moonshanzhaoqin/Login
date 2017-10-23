a2054d976072a64a33cb6d6688a51cfb37015db3
silent.sun <silent.sun@aspectgaming.com>
2017-10-23 10:18:11 +0800

    Signed-off-by: silent.sun <silent.sun@aspectgaming.com>

--- db/db_change_2.0.0.sql
+++ db/db_change_2.0.0.sql
@@ -1,5 +1,12 @@
 ALTER TABLE `e_transfer`     
-ADD COLUMN `goldpay_order_id` varchar(255) COMMENT 'goldpayOrderId' AFTER `paypal_exchange`
+ADD COLUMN `goldpay_order_id` varchar(255) COMMENT 'goldpayOrderId' AFTER `paypal_exchange`;
 
 ALTER TABLE `e_exchange`     
-ADD COLUMN `goldpay_order_id` varchar(255) COMMENT 'goldpayOrderId' AFTER `exchange_id`
+ADD COLUMN `goldpay_order_id` varchar(255) COMMENT 'goldpayOrderId' AFTER `exchange_id`;
+
+DELETE FROM `e_config` WHERE `config_key` = 'tpps_client_id';
+DELETE FROM `e_config` WHERE `config_key` = 'tpps_client_key';
+DELETE FROM `e_config` WHERE `config_key` = 'tpps_trans_token';
+
+增加系统Goldpay总账号
+INSERT INTO `e_bind` (`user_id`, `goldpay_id`, `goldpay_name`, `goldpay_acount`, `token`) VALUES ('1', '1', 'system', '111111111111', ''); 
