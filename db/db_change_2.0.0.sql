9b1a5ed65e73bfd89cefafd7d1a67d6558ab9c02
silent.sun <silent.sun@aspectgaming.com>
2017-10-23 10:03:51 +0800

    Signed-off-by: silent.sun <silent.sun@aspectgaming.com>

--- db/db_change_2.0.0.sql
+++ db/db_change_2.0.0.sql
@@ -1,7 +1,3 @@
-
--- add by niklaus.chi at 2017-10-16
-INSERT INTO `e_config` (`config_key`, `config_value`, `config_name`) VALUES ('goldpay_system_account', 'ssss', 'Goldpay系统账户')
-
 ALTER TABLE `e_transfer`     
 ADD COLUMN `goldpay_order_id` varchar(255) COMMENT 'goldpayOrderId' AFTER `paypal_exchange`
 
