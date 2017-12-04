2fc6af787e510ff5e5f6d904f5d12534a5c7b201
suzan.wu <suzan.wu@aspectgaming.com>
2017-12-04 18:33:32 +0800

    参数约束
    Signed-off-by: suzan.wu <suzan.wu@aspectgaming.com>

--- exchangeserver/src/main/java/com/yuyutechnology/exchange/server/controller/request/WithdrawConfirmRequset.java
+++ exchangeserver/src/main/java/com/yuyutechnology/exchange/server/controller/request/WithdrawConfirmRequset.java
@@ -1,5 +1,7 @@
 package com.yuyutechnology.exchange.server.controller.request;
 
+import org.apache.commons.lang.StringUtils;
+
 public class WithdrawConfirmRequset {
 	private int goldBullion;
 	private String userEmail;
@@ -18,5 +20,20 @@ public class WithdrawConfirmRequset {
 
 	public void setUserEmail(String userEmail) {
 		this.userEmail = userEmail;
+	}
+	
+	/**
+	 * 判断参数是否为空
+	 * 
+	 * @return
+	 */
+	public boolean empty() {
+		if (this.goldBullion <= 0) {
+			return true;
+		}
+		if (StringUtils.isBlank(this.userEmail)) {
+			return true;
+		}
+		return false;
 	}
 }
