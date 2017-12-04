bd791806cefbad6958b57fc01124a6889eea4ac9
silent.sun <silent.sun@aspectgaming.com>
2017-12-04 12:05:08 +0800

    增加获取通过id 获取手机和用户名
    Signed-off-by: silent.sun <silent.sun@aspectgaming.com>

---
+++ exchangeserver/src/main/java/com/yuyutechnology/exchange/server/controller/request/GetUserByIdRequest.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.yuyutechnology.exchange.server.controller.request;
+
+/**
+ * @author suzan.wu
+ *
+ */
+public class GetUserByIdRequest {
+	
+	private Long userId;
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	
+}
