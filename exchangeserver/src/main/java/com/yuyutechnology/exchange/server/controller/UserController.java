e626b794f1787c7b4b67a98a412b0409b8df4bc5
Niklaus Chi <nicholas.chi@aspectgaming.com>
2017-12-12 10:06:31 +0800

    新分支
    Signed-off-by: Niklaus Chi <nicholas.chi@aspectgaming.com>

--- exchangeserver/src/main/java/com/yuyutechnology/exchange/server/controller/UserController.java
+++ exchangeserver/src/main/java/com/yuyutechnology/exchange/server/controller/UserController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 import com.yuyutechnology.exchange.MessageConsts;
 import com.yuyutechnology.exchange.RetCodeConsts;
