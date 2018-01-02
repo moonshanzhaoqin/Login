2765c301063dc73369c0c52722886a3f2f12e7af
silent.sun <silent.sun@aspectgaming.com>
2018-01-02 14:05:57 +0800

    Signed-off-by: silent.sun <silent.sun@aspectgaming.com>

--- exchangeserver/src/main/java/com/yuyutechnology/exchange/server/controller/LoggedInUserController.java
+++ exchangeserver/src/main/java/com/yuyutechnology/exchange/server/controller/LoggedInUserController.java
@@ -359,8 +359,8 @@ public class LoggedInUserController {
 
 	@ResponseEncryptBody
 	@ApiOperation(value = "设置修改头像2 ", httpMethod = "POST", notes = "")
-	@RequestMapping(value = "/token/{token}/user/uploadPortrait2", method = RequestMethod.POST, produces = "application/json; charset=utf-8", consumes = "multipart/form-data")
-	public ModifyUserPortraitResponse uploadPortrait2(@PathVariable String token,
+	@RequestMapping(value = "/token/{token}/user/uploadPortrait", method = RequestMethod.POST, produces = "application/json; charset=utf-8", consumes = "multipart/form-data")
+	public ModifyUserPortraitResponse uploadPortrait(@PathVariable String token,
 			@RequestParam("file") MultipartFile multipartFile) throws HttpException, IOException {
 		logger.info("========uploadPortrait : {}============", token);
 		ModifyUserPortraitResponse rep = new ModifyUserPortraitResponse();
